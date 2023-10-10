4ee57c8935826a5372ad6ca66285d3f8d45f8aad
Salma <salmadoukkane@gmail.com>
2023-10-09 17:01:44 -0700

    1st commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all alphabet letters from 'a' to 'z' execpt e and q.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
