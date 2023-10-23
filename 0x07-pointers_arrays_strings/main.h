21292454c4edf543d8b6992a05da5f5226b6abbc
Salma <salmadoukkane@gmail.com>
2023-10-23 15:17:42 -0700

    hey

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char _memset(chars, char b, unsigned int n);
+char _memcpy(chardest, char src, unsigned int n);
+char_strchr(char s, char c);
+unsigned int _strspn(chars, char accept);
+char_strpbrk(char s, characcept);
+char _strstr(charhaystack, char needle);
+void print_chessboard(char (a)[8]);
+void print_diagsums(int a, int size);
+void set_string(char **s, charto);
+
+#endif
