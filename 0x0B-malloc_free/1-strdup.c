b5580528b10cedd7a8c9f03b6d43bba692eff735
Salma <salmadoukkane@gmail.com>
2023-10-31 16:16:51 -0700

    hey

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,13 +6,13 @@
  * @str: char
  * Return: 0
  */
-char *_strdup(char *str)	
+char *_strdup(char *str)
 {
 	char *aaa;
 	int i, r = 0;
 
 	if (str == NULL)
-		return (NULL);	
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
