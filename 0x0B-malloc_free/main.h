df7374c012b3a62be57e26a1c4a8bb2cb1236c7f
Salma <salmadoukkane@gmail.com>
2023-11-01 16:27:14 -0700

    hey

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,8 +7,8 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);		
-char wrdcnt(char *s);
+char *argstostr(int ac, char **av);
+int wrdcnt(char *s);
 int _strlen(char *s);
 char **strtow(char *str);
 
