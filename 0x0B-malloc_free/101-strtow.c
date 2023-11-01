4b35cc94b041810fedbebfe4d40b34404bf61510
Salma <salmadoukkane@gmail.com>
2023-10-31 17:05:02 -0700

    hey

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,8 +4,7 @@
  *  * count_word - helper function to count the number of words in a string
  *   * @s: string to evaluate
  *    *
- *     * Return: number of words
- *     
+ *     * Return: number of words   
  *     */
 int count_word(char *s)
 {
