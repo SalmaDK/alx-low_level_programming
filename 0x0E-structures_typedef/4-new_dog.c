3a6c4abd013f4269169c692dbc883787bc24676e
Salma <salmadoukkane@gmail.com>
2023-11-07 16:08:57 -0800

    me

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@ int _strlen(char *s)
 	int i;
 	i = 0;
 
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -33,6 +34,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 	len = 0;
+
 
 	while (src[len] != '\0')
 	{
