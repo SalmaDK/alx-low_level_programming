807769679a6a0e37d18931eedae182f168813593
SalmaDK <salmadoukkane@gmail.com>
2024-01-15 15:12:50 -0800

    hey

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: a pointer to the modified memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
+}
